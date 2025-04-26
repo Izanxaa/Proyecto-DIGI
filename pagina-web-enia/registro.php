aff74e65663930c0a90ab804d39b4785d35ba3e3
Izan <ifloressan@educacion.navarra.es>
2025-04-26 16:03:40 +0200

    añadir el php de control de usuarios del sistema

---
+++ pagina-web-enia/registro.php
@@ -0,0 +1,15 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "empresa_chatbot");
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$contraseña = password_hash($_POST['contraseña'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO usuarios (nombre, email, contraseña) VALUES ('$nombre', '$email', '$contraseña')";
+
+if ($conexion->query($sql)) {
+    echo "Usuario registrado con éxito.";
+} else {
+    echo "Error: " . $conexion->error;
+}
+?>
