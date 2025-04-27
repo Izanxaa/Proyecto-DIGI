d4734f17285fe47f5385ea3f7ca75234fba4a698
Izan <ifloressan@educacion.navarra.es>
2025-04-27 20:23:10 +0200

    .

--- pagina-web-elbarrio/registro.php
+++ pagina-web-elbarrio/registro.php
@@ -1,11 +1,11 @@
 <?php
- $conexion = new mysqli("localhost", "root", "", "empresa_chatbot");
+// $conexion = new mysqli("localhost", "root", "", "empresa_chatbot");
 
- $nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$contraseña = password_hash($_POST['contraseña'], PASSWORD_DEFAULT);
+// $nombre = $_POST['nombre'];
+//$email = $_POST['email'];
+//$contraseña = password_hash($_POST['contraseña'], PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO usuarios (nombre, email, contraseña) VALUES ('$nombre', '$email', '$contraseña')";
+//$sql = "INSERT INTO usuarios (nombre, email, contraseña) VALUES ('$nombre', '$email', '$contraseña')";
 
 if ($conexion->query($sql)) {
     echo "Usuario registrado con éxito.";
